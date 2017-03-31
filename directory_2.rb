46fa3a3153314a3bec1a5050d4fd5c24fe355a72
Sandy Cochrane <a.x.cochrane@gmail.com>
2017-03-31 19:18:35 +0100

    added user input feature to the program

--- directory_2.rb
+++ directory_2.rb
@@ -33,8 +33,24 @@ def print_footer names
 	puts "overall, we have #{names.count} great students"
 end
 
+def input_students
+	puts "Please enter the name of the students"
+	puts "To finish simply hit enter twice"
+
+	students = []
+	name = gets.chomp
+
+	while !name.empty? do
+		students << {name: name, cohort: :november}
+		puts "Now we have #{students.count} student(s)"
+		name = gets.chomp
+	end
+	students
+end
+
+students = input_students
 
 print_header
-print_names(students)
+puts(students)
 print_footer(students)
 
