ec84745743b396304e6a85a4a1fe8440a07665a5
Sandy Cochrane <a.x.cochrane@gmail.com>
2017-04-04 00:30:47 +0100

    fixed method errors

--- directory_2.rb
+++ directory_2.rb
@@ -1,26 +1,20 @@
-# create an array method
-#
-#
-#
-#
-#
 students = [
-	{name: "Dr. Hannibal Lecter", cohort: :november},
-	{name: "Darth Vader", cohort: :november},
-	{name: "Nurse Ratched", cohort: :november},
-	{name: "Michael Corleone", cohort: :november},
-	{name: "Alex DeLarge", cohort: :november},
-	{name: "The Wicked Witch of the West", cohort: :november},
-	{name: "Terminator", cohort: :november},
-	{name: "Freddy Krueger", cohort: :november},
-	{name: "The Joker", cohort: :november},
-	{name: "Joffrey Baratheon", cohort: :november},
-	{name: "Norman Bates", cohort: :november},
+	{name: "Dr. Hannibal Lecter", cohort: :november, hobbies: :eating, "country of birth": :UK, height: 172},
+	{name: "Darth Vader", cohort: :november, hobbies: :drinking, "country of birth": :UK, height: 173},
+	{name: "Nurse Ratched", cohort: :november, hobbies: :swimming, "country of birth": :UK, height: 174},
+	{name: "Michael Corleone", cohort: :november, hobbies: :running, "country of birth": :UK, height: 175},
+	{name: "Alex DeLarge", cohort: :november, hobbies: :eating, "country of birth": :Scotland, height: 176},
+	{name: "The Wicked Witch of the West", cohort: :november, hobbies: :football, "country of birth": :UK, height: 177},
+	{name: "Terminator", cohort: :november, hobbies: :cycling, "country of birth": :UK, height: 178},
+	{name: "Freddy Krueger", cohort: :november, hobbies: :eating, "country of birth": :UK, height: 179},
+	{name: "The Joker", cohort: :november, hobbies: :cycling, "country of birth": :UK, height: 180},
+	{name: "Joffrey Baratheon", cohort: :november, hobbies: :running, "country of birth": :UK, height: 181},
+	{name: "Norman Bates", cohort: :november, hobbies: :eating, "country of birth": :UK, height: 182},
 ]
 
 def print_header
-	puts "The students of Villains Academy"
-	puts "-------------"
+	puts "The students of Villains Academy".center(50)
+	puts "-------------".center(50)
 end
 
 def print_names names
@@ -29,38 +23,99 @@ def print_names names
 	end
 end
 
+def print_names_while(names)
+	count = 0
+	array_count = names.count
+		while count < array_count
+			puts names[count-1]
+			count += 1
+		end
+end
+
 def print_footer names
 	puts "overall, we have #{names.count} great students"
 end
 
-def print_names_condition names
+def print_names_c names
  		names.each_with_index do |name, index|
  			first_name = name[:name]
- 			if first_name[0] == "S"
-		puts "#{index}. #{name[:name]} (#{name[:cohort]} cohort)"
-			end
-	end
+	 			if first_name[0] == "S"
+					puts "#{index}. #{name[:name]} (#{name[:cohort]} cohort)"
+				end
+		end
 end
 
+def print_names_short names
+ 		names.each_with_index do |name, index|
+ 			first_name = name[:name]
+	 			if first_name.length <= 12
+					puts "#{index}. #{name[:name]} (#{name[:cohort]} cohort)"
+				end
+		end
+end
 
 def input_students
 	puts "Please enter the name of the students"
 	puts "To finish simply hit enter twice"
-
 	students = []
 	name = gets.chomp
-
-	while !name.empty? do
-		students << {name: name, cohort: :november}
-		puts "Now we have #{students.count} student(s)"
-		name = gets.chomp
-	end
+		while !name.empty? do
+			students << {name: name, cohort: :november}
+			puts "Now we have #{students.count} student(s)"
+			name = gets.chomp
+		end
 	students
 end
 
-students = input_students
+def input_students_v2
+	students = []
+	add_mode = true
+		while add_mode
+			puts "Please enter the name of student (cannot be blank)"
+			name = gets.chomp
+			  while name.empty?
+					puts "Please enter the name of student (cannot be blank)"
+					name = gets.chomp
+				end
+			
+			puts "Please enter student start cohort (if blank will default to January)"
+			x = gets.chomp
+				if x.empty?
+					cohort = :january
+				else
+					cohort = x.to_sym
+				end
+			
+			puts "Did you make a typo (y/n)"
+			typo = gets.chomp
+				if typo == 'n'
+					students << {name: name, cohort: cohort}
+					puts "Now we have #{students.count} student(s)"
+					puts students
+				else 
+					puts "ok, cancelling input mode"
+				end
 
+		 	puts "Add another student (y/n)"
+				answer = gets.chomp
+				if answer == 'n' 
+					add_mode = false
+				end
+		end
+	return students
+end
+
+# ask name
+#   check if blank, insist on name
+# ask cohort
+#   if cohort is blank assign defaault month jan
+# ask if typo
+#   if typo don't shovel
+#
+
+
+students = input_students_v2
 print_header
-print_names_condition(students)
+print_names_while(students)
 print_footer(students)
 
